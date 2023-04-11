d1437aed76438e1aeb44ff3b5140fdb5564bcd81
Inaki Villar <inaki.seri@gmail.com>
2023-04-10 17:15:58 -0700

    a

---
+++ library/src/test/java/com/example/library/AnotherTest.java
@@ -0,0 +1,24 @@
+package com.example.library;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.android.controller.ActivityController;
+
+@RunWith(RobolectricTestRunner.class)
+public class WelcomeActivityTest {
+
+    @Test
+    public void clickingLogin_shouldStartLoginActivity() {
+    //    try (ActivityController<MainActivity> controller = Robolectric.buildActivity(MainActivity.class)) {
+//            controller.setup(); // Moves Activity to RESUMED state
+//            MainActivity activity = controller.get();
+//
+//            activity.findViewById(R.id.login).performClick();
+////            Intent expectedIntent = new Intent(activity, LoginActivity.class);
+////            Intent actual = shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
+////            assertEquals(expectedIntent.getComponent(), actual.getComponent());
+        }
+    }
+}
